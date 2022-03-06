f83888748df6421016f3bd6b04212f9e93ac188e
Jasonleeo-cpu <71202368+Jasonleeo-cpu@users.noreply.github.com>
2022-03-06 09:45:14 +0800

    Add files via upload

---
+++ Real_ws/run.sh
@@ -0,0 +1,6 @@
+#roscore & sleep 5;
+roslaunch vicon_bridge vicon.launch & sleep 3;
+roslaunch mavros px4.launch & sleep 3;
+roslaunch ekf PX4_vicon.launch & sleep 3;
+roslaunch px4ctrl run_ctrl.launch 
+#rosbag record /vicon_imu_ekf_odom /debugPx4ctrl
